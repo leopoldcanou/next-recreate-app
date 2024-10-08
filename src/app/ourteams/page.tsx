9e460567286f24a7801f701cc417ddf801e6cb81
Poldus <leopold.canou@gmail.com>
2024-10-08 10:49:38 +0200

    remove react router dom

--- src/app/ourteams/page.tsx
+++ src/app/ourteams/page.tsx
@@ -1,40 +1,37 @@
-import Team from '../ui/Team';
-import TeamSkeleton from '../ui/Team/TeamSkeleton.jsx';
-import Testimonial from '../ui/Testimonial';
+import Team from "../ui/Team";
+import TeamSkeleton from "../ui/Team/TeamSkeleton.jsx";
+import Testimonial from "../ui/Testimonial";
 
-import { useLoaderData, defer, Await } from 'react-router-dom';
-import {fetchOurTeams, fetchTestimonialData} from '../lib/loaders.js';
-import { Suspense } from 'react';
-
+import { fetchOurTeams, fetchTestimonialData } from "../lib/loaders.js";
+import { Suspense } from "react";
 
 // Version de base, monocomposant et sans gestion defer/Await
 
-export async function loader({params}){
-    const ourteams = await fetchOurTeams(params.teamName);
-    return ourteams;
-}   
+export async function loader({ params }) {
+  const ourteams = await fetchOurTeams(params.teamName);
+  return ourteams;
+}
 
-export default function OurTeams(){
-    
-    const data = useLoaderData();
-    
-    return (
-        <section>
-            <Team {...data}  />
-        </section>
-    );
-}   
+export default function OurTeams() {
+  const data = useLoaderData();
+
+  return (
+    <section>
+      <Team {...data} />
+    </section>
+  );
+}
 
 // export async function loader({params}){
 //     const ourteams = fetchOurTeams(params.teamName);
 //     const testimonialData = await fetchTestimonialData(params.teamName);
 //     return defer({team: ourteams, testimonial: testimonialData});
-// }   
+// }
 
 // export default function OurTeams(){
-    
+
 //     const data = useLoaderData();
-    
+
 //     return (
 //         <section>
 //             <Suspense fallback={<TeamSkeleton / >}>
@@ -46,4 +43,4 @@ export default function OurTeams(){
 //             <Testimonial data={data.testimonial}/>
 //         </section>
 //     );
-// }   
+// }
